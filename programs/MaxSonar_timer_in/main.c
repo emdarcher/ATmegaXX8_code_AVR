692353f1f7acfdd9bd384c64650bc1b93685b0d0
Ethan Durrant <emd519@yahoo.com>
2015-01-17 23:55:46 -0800

    things

--- programs/MaxSonar_timer_in/main.c
+++ programs/MaxSonar_timer_in/main.c
@@ -15,16 +15,16 @@ void main(void){
     /* init stuff */
     DDRD |= (1<<PD1);
     DDRB |= (1<<PB5);
-//   DDRE |= (1<<PE1);
+//    DDRE |= (1<<PE1);
     uart0_init(UART_BAUD_SELECT(BAUD, F_CPU));
 
     sei();
     /* infinite loop */
     while(1){
-    uart0_puts_p(&hw_string);
-//   PORTE ^= (1<<PE1);
-    PORTB ^= (1<<PB5);
-    _delay_ms(1000);     
+        uart0_puts_p(&hw_string);
+    //   PORTE ^= (1<<PE1);
+        PORTB ^= (1<<PB5);
+        _delay_ms(1000);     
     }
 }
 
