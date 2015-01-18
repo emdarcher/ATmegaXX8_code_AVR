d915bc5b1cd4004a988728ac1984072565ec620d
Ethan Durrant <emd519@yahoo.com>
2015-01-17 16:31:28 -0800

    stuff for MaxSonar sensor

---
+++ programs/MaxSonar_timer_in/MaxSonar_EZ1.c
@@ -0,0 +1,9 @@
+
+
+#include "MaxSonar_EZ1.h"
+
+
+inline uint8_t ez1_us_to_inches(uint16_t in_us){
+    return in_us/147;
+}
+
---
+++ programs/MaxSonar_timer_in/MaxSonar_EZ1.h
@@ -0,0 +1,12 @@
+
+
+#ifndef _MAXSONAR_EZ1_H_
+#define _MAXSONAR_EZ1_H_
+
+#define EZ1_US_TO_INCHES(us) ((us)/147)
+
+extern 
+inline uint8_t ez1_us_to_inches(uint16_t in_us); 
+
+
+#endif
